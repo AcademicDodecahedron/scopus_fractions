148e7d497cf73a301ebe5235b6dd51b8980d4ba4
francisthebasilisk <snorkysnark@gmail.com>
2021-08-04 16:59:21 +0300

    Revert "open temlate as read only"
    This reverts commit d534c8b79ae6db972f6f9b9b79ec92d34d9f8d8b.

--- generate.py
+++ generate.py
@@ -19,7 +19,7 @@ def parse_args():
     return argp.parse_args()
 args = parse_args()
 
-wb = openpyxl.load_workbook(args.template, read_only=True)
+wb = openpyxl.load_workbook(args.template)
 id_aff_trans = wb['id_aff_trans']
 fractions = wb['фракции']
 publications = wb['публикации']
